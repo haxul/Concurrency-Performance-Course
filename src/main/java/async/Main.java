ba7fc74c7991215a560d0968444f6f019f1f9290
haxul <s.s.starodubov@yandex.ru>
2020-09-03 22:51:00 +0400

    async

--- src/main/java/async/Main.java
+++ src/main/java/async/Main.java
@@ -14,59 +14,57 @@ import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException, ParseException, FileNotFoundException, ExecutionException, InterruptedException {
-
+        List<String> urls = List.of(
+                "/home/haxul/Development/cuncurrency-course/src/main/java/async/source.json",
+                "/home/haxul/Development/cuncurrency-course/src/main/java/async/source2.json"
+        );
         JsonReader jsonReader = new JsonReader();
-//        List<String> jsonArrays = jsonReader.completableFutureAllof(List.of(
-//                "/home/haxul/Development/cuncurrency-course/src/main/java/async/source.json",
-//                "/home/haxul/Development/cuncurrency-course/src/main/java/async/source2.json"
-//        ));
-        jsonReader.tester();
+        jsonReader.readJsonFiles(urls);
     }
 }
 
 
 class JsonReader {
 
-    public void tester() {
-        List<String> urls = List.of(
-                "/home/haxul/Development/cuncurrency-course/src/main/java/async/source.json",
-                "/home/haxul/Development/cuncurrency-course/src/main/java/async/source2.json"
-        );
-
-        List<CompletableFuture<JSONArray>> futures = new ArrayList<>();
-        for (var url : urls) {
-            CompletableFuture<JSONArray> future = CompletableFuture.supplyAsync(() -> {
-                try {
-                    if (url.equals( "/home/haxul/Development/cuncurrency-course/src/main/java/async/source2.json")) Thread.sleep(3000);
-                    JSONParser jsonParser = new JSONParser();
-                    return (JSONArray) jsonParser.parse(new FileReader(url));
-                } catch (Exception e) {
-                    System.err.println(e.getMessage());
-                    return null;
-                }
-            });
-            futures.add(future);
+    public JSONArray tryParseJsonArray(String url) {
+        try {
+            JSONParser jsonParser = new JSONParser();
+            return (JSONArray) jsonParser.parse(new FileReader(url));
+        } catch (Exception r) {
+            return null;
         }
-        List<JSONArray> collect = futures.stream().map(future -> {
-            try {
-                return future.get(2, TimeUnit.SECONDS);
-            } catch (Exception e) {
-                return null;
-            }
-        }).filter(Objects::nonNull).collect(Collectors.toList());
+    }
+
+    public JSONArray tryParseJsonArray(CompletableFuture<JSONArray> future) {
+        try {
+            return future.get(2, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public void readJsonFiles(List<String> urls) {
+
+        List<CompletableFuture<JSONArray>> futures = urls.stream()
+                .map((url) -> CompletableFuture.supplyAsync(()-> tryParseJsonArray(url)))
+                .collect(Collectors.toList());
+
+        List<JSONArray> collect = futures.stream()
+                .map(this::tryParseJsonArray)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+
         List<Long> result = new ArrayList<>();
         for (JSONArray jsonArray : collect) {
-           for (Object item : jsonArray) {
-               JSONObject el = (JSONObject) item;
-               Long id = (Long) el.get("id");
-               result.add(id);
-           }
+            for (Object item : jsonArray) {
+                JSONObject el = (JSONObject) item;
+                Long id = (Long) el.get("id");
+                result.add(id);
+            }
         }
         result.sort((Long::compareTo));
         System.out.println(result);
