54cfdb699605ada5699c67990b11e7abefa7d1f7
haxul <s.s.starodubov@yandex.ru>
2020-09-07 14:44:03 +0400

    string test

--- src/main/java/string/StringTest.java
+++ src/main/java/string/StringTest.java
@@ -2,20 +2,21 @@ package string;
 
 public class StringTest {
     public static void main(String[] args) {
-        long start = System.nanoTime();
 
+
+        long start2 = System.nanoTime();
+        StringBuilder builder = new StringBuilder();
+        String hello_world = builder.append("helloworld").append("hello world").toString();
+        System.out.println(hello_world);
+        System.out.println((System.nanoTime() - start2) / 1000);
+
+
+        long start = System.nanoTime();
         String init = new String("helloworld");
 
         init += "hello orld";
         System.out.println(init);
         System.out.println((System.nanoTime() - start) / 1000);
-
-        long start2 = System.nanoTime();
-        String init2 = new String("helloworld");
-        StringBuilder builder = new StringBuilder(init);
-        String hello_world = builder.append("hello world").toString();
-        System.out.println(hello_world);
-        System.out.println((System.nanoTime() - start2) / 1000);
 
     }
 }
